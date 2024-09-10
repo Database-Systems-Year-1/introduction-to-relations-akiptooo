ce65ebe73c570b2bcc93541993c3af1ce469ca13
Andrew Kiptoo <167400953+akiptooo@users.noreply.github.com>
2024-09-10 22:44:36 +0300

    Update table2.sql

--- table2.sql
+++ table2.sql
@@ -2,7 +2,8 @@ create table employee (
     EmployeeID INT PRIMARY KEY,
     FirstName VARCHAR(100),
     LastName VARCHAR(100));
-    
+    insert into employee(EmployeeID, FirstName,LastName)
+    VALUES(001, Andre, Philip);
 select EmployeeID, FirstName from employee;
 alter table employee
 add column Email VARCHAR(255);
