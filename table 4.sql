b175dac7ddb3eb5c50dce024de93d9b3a1e52557
Andrew Kiptoo <167400953+akiptooo@users.noreply.github.com>
2024-09-10 22:50:42 +0300

    Create table 4.sql

---
+++ table 4.sql
@@ -0,0 +1,16 @@
+create table ordered (
+    OrderID INT PRIMARY KEY,
+    OrderDate VARCHAR(50),
+    CustomerID INT,
+    TotalAmount DECIMAL(10, 2));
+	insert into ordered(OrderID, OrderDate, CustomerID, TotalAmount)
+	VALUES(001, 'Tuesday', 257, 500);
+
+	VALUES 
+
+   alter table ordered
+   alter column OrderDate type VARCHAR(100);
+   Select *from ordered
+   
+
+   
