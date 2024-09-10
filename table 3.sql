eb8a27867c001f04e7c5a8a5f16287ea29313eac
Andrew Kiptoo <167400953+akiptooo@users.noreply.github.com>
2024-09-10 22:42:32 +0300

    table 3.sql

--- table 3.sql
+++ table 3.sql
@@ -3,5 +3,7 @@ create table Customers( (
     FirstName VARCHAR(100),
     LastName VARCHAR(100),
     Email VARCHAR(255));
+insert into Customers(CustomerID, FirstName, LastName, Email)
+VALUES(001, Andre, Philip, ap@gmail.com);
 
 	drop table Customers;
